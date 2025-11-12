2ef0fb95f52584191a2219b14f9b55dbabd92821
Bekbol <bekbolinr@gmail.com>
2025-11-12 20:17:46 +0600

    Add test_catalog URL route

--- quiz/urls.py
+++ quiz/urls.py
@@ -6,51 +6,63 @@ app_name = 'quiz'
 
 
 urlpatterns = [
-    path('', views.main, name='main'),
-    path('profile/', views.profile, name='profile'),
-    path('create/', views.select_test_type, name='select_test_type'),
+        path('', views.main, name='main'),
+        path('profile/', views.profile, name='profile'),
+        path('create/', views.select_test_type, name='select_test_type'),
 
-    path('create/<str:test_type>/', views.create_test, name='create_test'),
-
-
-    path('create-mixed-test/<int:test_id>/', views.create_mixed_test, name='create_mixed_test'),
-    path('create/text/<int:test_id>/', views.create_text_test, name='create_text_test'),
-    path('create/voice/<int:test_id>/', views.create_voice_test, name='create_voice_test'),
-    path('create/photo/<int:test_id>/', views.create_photo_test, name='create_photo_test'),
-    path('create/math/<int:test_id>/', views.create_math_test, name='create_math_test'),
+        path('create/<str:test_type>/', views.create_test, name='create_test'),
 
 
-    path('created/<uuid:public_id>/', views.test_created, name='test_created'),
-    path('take/<uuid:public_id>/thanks/', views.take_thanks, name='take_thanks'),
-
-    path('<int:test_id>/', views.test_detail, name='test_detail'),
-    path('delete/<int:test_id>/', views.delete_test, name='delete_test'),
-
-    path('delete/<uuid:public_id>/', views.delete_test, name='delete_test'),
-
-    path('take/photo/<uuid:public_id>/', views.take_photo_test, name='take_photo_test'),
-    path('take/voice/<uuid:public_id>/', views.take_voice_test, name='take_voice_test'),
-    path('take/text/<uuid:public_id>/', views.take_text_test, name='take_text_test'),
-    path('take/<uuid:public_id>/', views.take_test, name='take_test'),
-    path('math-test/<uuid:public_id>/', views.take_math_test, name='take_math_test'),
-    path('test/<uuid:public_id>/mixed/', views.take_mixed_test, name='take_mixed_test'),
-    path('result/<int:result_id>/', views.show_result, name='show_result'),
-    path('process-results/<uuid:public_id>/', views.process_test_results, name='process_test_results'),
+        path('create-mixed-test/<int:test_id>/', views.create_mixed_test, name='create_mixed_test'),
+        path('create/text/<int:test_id>/', views.create_text_test, name='create_text_test'),
+        path('create/voice/<int:test_id>/', views.create_voice_test, name='create_voice_test'),
+        path('create/photo/<int:test_id>/', views.create_photo_test, name='create_photo_test'),
+        path('create/math/<int:test_id>/', views.create_math_test, name='create_math_test'),
 
 
+        path('created/<uuid:public_id>/', views.test_created, name='test_created'),
+        path('take/<uuid:public_id>/thanks/', views.take_thanks, name='take_thanks'),
 
-    path('student-results/', views.student_results, name='student_results'),
-    path('student-results/<uuid:public_id>/', views.test_student_results, name='test_student_results'),
-    path('classes/', views.class_groups, name='class_groups'),
-    path('classes/<int:class_id>/', views.class_group_detail, name='class_group_detail'),
+        path('<int:test_id>/', views.test_detail, name='test_detail'),
+        path('delete/<int:test_id>/', views.delete_test, name='delete_test'),
+
+        path('delete/<uuid:public_id>/', views.delete_test, name='delete_test'),
+
+        path('take/photo/<uuid:public_id>/', views.take_photo_test, name='take_photo_test'),
+        path('take/voice/<uuid:public_id>/', views.take_voice_test, name='take_voice_test'),
+        path('take/text/<uuid:public_id>/', views.take_text_test, name='take_text_test'),
+
+        path('take/<uuid:public_id>/', views.take_test, name='take_test'),
+
+
+        path('math-test/<uuid:public_id>/', views.take_math_test, name='take_math_test'),
+        path('test/<uuid:public_id>/mixed/', views.take_mixed_test, name='take_mixed_test'),
+        path('result/<int:result_id>/', views.show_result, name='show_result'),
+        path('process-results/<uuid:public_id>/', views.process_test_results, name='process_test_results'),
 
 
 
-    path('math-test/<uuid:public_id>/', views.take_math_test, name='take_math_test'),
-    path('math-thanks/<uuid:public_id>/', views.take_math_thanks, name='take_math_thanks'),
+        path('student-results/', views.student_results, name='student_results'),
+        path('student-results/<uuid:public_id>/', views.test_student_results, name='test_student_results'),
+        path('classes/', views.class_groups, name='class_groups'),
+        path('classes/<int:class_id>/', views.class_group_detail, name='class_group_detail'),
 
-    path('student-result-details/<int:result_id>/', views.student_result_details, name='student_result_details'),
-    path('test-json/<int:result_id>/', views.test_json, name='test_json'),
+
+
+        path('math-test/<uuid:public_id>/', views.take_math_test, name='take_math_test'),
+        path('math-thanks/<uuid:public_id>/', views.take_math_thanks, name='take_math_thanks'),
+
+        path('student-result-details/<int:result_id>/', views.student_result_details, name='student_result_details'),
+        path('test-json/<int:result_id>/', views.test_json, name='test_json'),
+
+        path('catalog/', views.test_catalog, name='test_catalog'),
+        path('publish-test/<int:test_id>/', views.publish_test, name='publish_test'),
+        path('unpublish-test/<int:test_id>/', views.unpublish_test, name='unpublish_test'),
+
+        path('edit-test/<int:test_id>/', views.edit_test, name='edit_test'),
+        path('copy-test/<int:test_id>/', views.copy_test, name='copy_test'),
+
+        path('edit-test-base/<int:test_id>/', views.edit_test_base, name='edit_test_base'),
 
 ]
 
